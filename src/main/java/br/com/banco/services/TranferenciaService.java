eac33a8d282c2b0c24e2523ecd926cd3eaf82715
Syllas Braga <syllasbraga2@gmail.com>
2023-07-13 10:17:17 -0300

    refact: Nomeção de uma constante

--- src/main/java/br/com/banco/services/TranferenciaService.java
+++ src/main/java/br/com/banco/services/TranferenciaService.java
@@ -2,17 +2,14 @@ package br.com.banco.services;
 
 import br.com.banco.dtos.TransferenciaDto;
 import br.com.banco.entities.Conta;
-import br.com.banco.entities.Transferencia;
 import br.com.banco.mapper.TransferenciaMapper;
 import br.com.banco.repositories.TransferenciaRepository;
-import br.com.banco.utils.CalculaSaldoUtil;
 import br.com.banco.utils.DataUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @Service
@@ -22,7 +19,7 @@ public class TranferenciaService {
     private final ContaService contaService;
     private final TransferenciaMapper transferenciaMapper;
     private final DataUtil dataUtil;
-    private static final int numeroItensPagina = 4;
+    private static final int NUMERO_ITENS_PAGINA = 4;
 
     public TranferenciaService(TransferenciaRepository transferenciaRepository, ContaService contaService,
                                TransferenciaMapper transferenciaMapper, DataUtil dataUtil) {
@@ -34,7 +31,7 @@ public class TranferenciaService {
 
     public Page<TransferenciaDto> buscaPeloNumeroConta(Long idConta, int page){
         Conta conta = contaService.iniciarConta(idConta);
-        Pageable pageable = PageRequest.of(page, numeroItensPagina);
+        Pageable pageable = PageRequest.of(page, NUMERO_ITENS_PAGINA);
 
         return transferenciaMapper.paraPageTransferenciaDto(transferenciaRepository.findByConta(conta, pageable));
     }
@@ -44,7 +41,7 @@ public class TranferenciaService {
         Timestamp dataFimFormatada = dataUtil.formataData(dataFim);
         Conta conta = contaService.iniciarContaPeriodo(idConta, dataInicioFormatada, dataFimFormatada);
 
-        Pageable pageable = PageRequest.of(page, numeroItensPagina);
+        Pageable pageable = PageRequest.of(page, NUMERO_ITENS_PAGINA);
 
         return transferenciaMapper.paraPageTransferenciaDto(
                 transferenciaRepository.findByContaAndDataTransferenciaBetween(conta, dataInicioFormatada,
@@ -53,7 +50,7 @@ public class TranferenciaService {
 
     public Page<TransferenciaDto> buscaPorNomeOperador(Long idConta, int page, String nomeOperador){
         Conta conta = contaService.iniciarConta(idConta);
-        Pageable pageable = PageRequest.of(page, numeroItensPagina);
+        Pageable pageable = PageRequest.of(page, NUMERO_ITENS_PAGINA);
 
         return transferenciaMapper.paraPageTransferenciaDto(
                 transferenciaRepository.findByContaAndNomeOperadorTransacao(conta, nomeOperador, pageable));
@@ -64,7 +61,7 @@ public class TranferenciaService {
         Timestamp dataInicioFormatada = dataUtil.formataData(dataInicio);
         Timestamp dataFimFormatada = dataUtil.formataData(dataFim);
         Conta conta = contaService.iniciarContaPeriodo(idConta, dataInicioFormatada, dataFimFormatada);
-        Pageable pageable = PageRequest.of(page, numeroItensPagina);
+        Pageable pageable = PageRequest.of(page, NUMERO_ITENS_PAGINA);
 
         return transferenciaMapper.paraPageTransferenciaDto(
                 transferenciaRepository.findByContaAndNomeOperadorTransacaoAndDataTransferenciaBetween(

d9248ef5ad2ac28acc12a72bae43770587651245
Syllas Braga <syllasbraga2@gmail.com>
2023-07-15 13:15:06 -0300

    refact: URL da ContaController

--- src/main/java/br/com/banco/controllers/ContaController.java
+++ src/main/java/br/com/banco/controllers/ContaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/conta")
+@RequestMapping("/contas")
 public class ContaController {
 
     private final ContaService contaService;
